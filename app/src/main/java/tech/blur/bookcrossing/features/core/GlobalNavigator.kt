eadd003616c642f542a87e209b05b2d78c19353b
Sergey Grechishnikov <Srg.Grch@gmail.com>
2018-09-19 00:36:06 +0700

    GlobalNavigator converted to Kotlin

---
+++ app/src/main/java/tech/blur/bookcrossing/features/core/GlobalNavigator.kt
@@ -0,0 +1,25 @@
+package tech.blur.bookcrossing.features.core
+
+import android.app.Activity
+import android.content.Intent
+
+import ru.terrakok.cicerone.Navigator
+import ru.terrakok.cicerone.commands.Command
+import ru.terrakok.cicerone.commands.Forward
+import tech.blur.bookcrossing.features.books.bookshelf.BookShelfActivity
+
+class GlobalNavigator(private val activity: Activity) : Navigator {
+
+    override fun applyCommands(commands: Array<Command>) {
+        if (commands[0] is Forward) {
+            val intent: Intent
+            when ((commands[0] as Forward).screenKey) {
+                Screens.BOOK_SHELF -> {
+                    intent = Intent(activity, BookShelfActivity::class.java)
+                    activity.startActivity(intent)
+                    activity.finish()
+                }
+            }
+        }
+    }
+}
